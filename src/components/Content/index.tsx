7735f5ff966e130338acd3a68df01558037c2a5d
vasconcelosjean42 <vasconcelosjean@acad.ifma.edu.br>
2024-01-17 22:14:45 -0300

    #1 trazendo api com axios

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,20 +1,21 @@
 import { ElementType, ReactNode, useContext, useEffect, useState } from "react";
 import Filters from "../Filter";
 import Card from "../Card";
-import SouthParkTheStickOfTruth from "../../assets/south-park-the-stick-of-truth.png";
-import FarCry from "../../assets/far-cry-3.jpg";
-import CallOfDuty from "../../assets/call-of-duty.png";
-import BaldursGate from "../../assets/baldurs-gate.png";
+// import SouthParkTheStickOfTruth from "../../assets/south-park-the-stick-of-truth.png";
+// import FarCry from "../../assets/far-cry-3.jpg";
+// import CallOfDuty from "../../assets/call-of-duty.png";
+// import BaldursGate from "../../assets/baldurs-gate.png";
 
-import {
-  FaWindows,
-  FaApple,
-  FaLinux,
-  FaPlaystation,
-  FaXbox,
-  FaAndroid,
-} from "react-icons/fa";
-import { BsNintendoSwitch } from "react-icons/bs";
+// import {
+//   FaWindows,
+//   FaApple,
+//   FaLinux,
+//   FaPlaystation,
+//   FaXbox,
+//   FaAndroid,
+// } from "react-icons/fa";
+// import { BsNintendoSwitch } from "react-icons/bs";
+import ApiClient from "../../services/api-client.ts";
 import { StyledContent } from "./style";
 import { Context } from "../../App.tsx";
 import { HStack, Wrap } from "../../../styled-system/jsx";
@@ -108,104 +109,110 @@ const Content = ({ genreName, handleGenre }: Props) => {
     return b.relevance - a.relevance;
   };
 
+  // useEffect(() => {
+  //   setGames([
+  //     {
+  //       name: "South Park",
+  //       image: SouthParkTheStickOfTruth,
+  //       relevance: 92,
+  //       icon: "🎯",
+  //       releaseDate: new Date(2010, 10, 4),
+  //       dateAdded: new Date(Date.now()),
+  //       genres: "RPG",
+  //       plataforms: [
+  //         { plataformName: "Windows", plataformIcon: FaWindows },
+  //         { plataformName: "Mac", plataformIcon: FaApple },
+  //         { plataformName: "Linux", plataformIcon: FaLinux },
+  //         { plataformName: "PS4", plataformIcon: FaPlaystation },
+  //         { plataformName: "Xbox", plataformIcon: FaXbox },
+  //         { plataformName: "Nintendo", plataformIcon: FaAndroid },
+  //         { plataformName: "Android", plataformIcon: BsNintendoSwitch },
+  //       ],
+  //     },
+  //     {
+  //       name: "FarCry",
+  //       image: FarCry,
+  //       relevance: 90,
+  //       icon: "🎯",
+  //       releaseDate: new Date(2010, 10, 2),
+  //       dateAdded: new Date(Date.now()),
+  //       genres: "Action",
+  //       plataforms: [
+  //         { plataformName: "Windows", plataformIcon: FaWindows },
+  //         { plataformName: "Linux", plataformIcon: FaLinux },
+  //         { plataformName: "PS4", plataformIcon: FaPlaystation },
+  //         { plataformName: "Nintendo", plataformIcon: FaAndroid },
+  //         { plataformName: "Android", plataformIcon: BsNintendoSwitch },
+  //       ],
+  //     },
+  //     {
+  //       name: "CallOfDuty",
+  //       image: CallOfDuty,
+  //       relevance: 89,
+  //       icon: "🎯",
+  //       releaseDate: new Date(2010, 10, 1),
+  //       dateAdded: new Date(Date.now()),
+  //       genres: "Shooter",
+  //       plataforms: [
+  //         { plataformName: "Windows", plataformIcon: FaWindows },
+  //         { plataformName: "Mac", plataformIcon: FaApple },
+  //         { plataformName: "Linux", plataformIcon: FaLinux },
+  //         { plataformName: "PS4", plataformIcon: FaPlaystation },
+  //         { plataformName: "Xbox", plataformIcon: FaXbox },
+  //         { plataformName: "Nintendo", plataformIcon: FaAndroid },
+  //         { plataformName: "Android", plataformIcon: BsNintendoSwitch },
+  //       ],
+  //     },
+  //     {
+  //       name: "Baldurs Gate",
+  //       image: BaldursGate,
+  //       relevance: 99,
+  //       icon: "🎲",
+  //       releaseDate: new Date(2010, 10, 3),
+  //       dateAdded: new Date(Date.now()),
+  //       genres: "RPG",
+  //       plataforms: [
+  //         { plataformName: "Windows", plataformIcon: FaWindows },
+  //         { plataformName: "Linux", plataformIcon: FaLinux },
+  //         { plataformName: "PS4", plataformIcon: FaPlaystation },
+  //         { plataformName: "Android", plataformIcon: BsNintendoSwitch },
+  //       ],
+  //     },
+  //   ]);
+  //   setPlataforms({
+  //     id: "plataform",
+  //     name: "plataform",
+  //     disableHiddenOption: "Plataforms",
+  //     options: [
+  //       "Windows",
+  //       "Mac",
+  //       "Linux",
+  //       "PS4",
+  //       "Xbox",
+  //       "Nintendo",
+  //       "Android",
+  //       "iOS",
+  //     ],
+  //   });
+  //   setOrders({
+  //     id: "orders",
+  //     name: "orders",
+  //     options: [
+  //       "Relevance",
+  //       "Date added",
+  //       "Name",
+  //       "Release date",
+  //       "Popularity",
+  //       "Average rating",
+  //     ],
+  //     children: "Order by: ",
+  //   });
+  // }, []);
+
   useEffect(() => {
-    setGames([
-      {
-        name: "South Park",
-        image: SouthParkTheStickOfTruth,
-        relevance: 92,
-        icon: "🎯",
-        releaseDate: new Date(2010, 10, 4),
-        dateAdded: new Date(Date.now()),
-        genres: "RPG",
-        plataforms: [
-          { plataformName: "Windows", plataformIcon: FaWindows },
-          { plataformName: "Mac", plataformIcon: FaApple },
-          { plataformName: "Linux", plataformIcon: FaLinux },
-          { plataformName: "PS4", plataformIcon: FaPlaystation },
-          { plataformName: "Xbox", plataformIcon: FaXbox },
-          { plataformName: "Nintendo", plataformIcon: FaAndroid },
-          { plataformName: "Android", plataformIcon: BsNintendoSwitch },
-        ],
-      },
-      {
-        name: "FarCry",
-        image: FarCry,
-        relevance: 90,
-        icon: "🎯",
-        releaseDate: new Date(2010, 10, 2),
-        dateAdded: new Date(Date.now()),
-        genres: "Action",
-        plataforms: [
-          { plataformName: "Windows", plataformIcon: FaWindows },
-          { plataformName: "Linux", plataformIcon: FaLinux },
-          { plataformName: "PS4", plataformIcon: FaPlaystation },
-          { plataformName: "Nintendo", plataformIcon: FaAndroid },
-          { plataformName: "Android", plataformIcon: BsNintendoSwitch },
-        ],
-      },
-      {
-        name: "CallOfDuty",
-        image: CallOfDuty,
-        relevance: 89,
-        icon: "🎯",
-        releaseDate: new Date(2010, 10, 1),
-        dateAdded: new Date(Date.now()),
-        genres: "Shooter",
-        plataforms: [
-          { plataformName: "Windows", plataformIcon: FaWindows },
-          { plataformName: "Mac", plataformIcon: FaApple },
-          { plataformName: "Linux", plataformIcon: FaLinux },
-          { plataformName: "PS4", plataformIcon: FaPlaystation },
-          { plataformName: "Xbox", plataformIcon: FaXbox },
-          { plataformName: "Nintendo", plataformIcon: FaAndroid },
-          { plataformName: "Android", plataformIcon: BsNintendoSwitch },
-        ],
-      },
-      {
-        name: "Baldurs Gate",
-        image: BaldursGate,
-        relevance: 99,
-        icon: "🎲",
-        releaseDate: new Date(2010, 10, 3),
-        dateAdded: new Date(Date.now()),
-        genres: "RPG",
-        plataforms: [
-          { plataformName: "Windows", plataformIcon: FaWindows },
-          { plataformName: "Linux", plataformIcon: FaLinux },
-          { plataformName: "PS4", plataformIcon: FaPlaystation },
-          { plataformName: "Android", plataformIcon: BsNintendoSwitch },
-        ],
-      },
-    ]);
-    setPlataforms({
-      id: "plataform",
-      name: "plataform",
-      disableHiddenOption: "Plataforms",
-      options: [
-        "Windows",
-        "Mac",
-        "Linux",
-        "PS4",
-        "Xbox",
-        "Nintendo",
-        "Android",
-        "iOS",
-      ],
-    });
-    setOrders({
-      id: "orders",
-      name: "orders",
-      options: [
-        "Relevance",
-        "Date added",
-        "Name",
-        "Release date",
-        "Popularity",
-        "Average rating",
-      ],
-      children: "Order by: ",
-    });
+    ApiClient.get<{ name: string[] }>("/games").then((res) =>
+      console.log(res.data.results)
+    );
   }, []);
 
   return (
